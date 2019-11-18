c28f103db794ba6df595c24fda0d6fe570995cba
Maxim Anisimov <maximanisimov1@gmail.com>
2019-11-18 23:30:47 +0200

    Added songs.js

---
+++ Homework #1/songs.js
@@ -0,0 +1,31 @@
+/* This file provides information about songs collection.
+   Here you can find information about artist, genre,
+   title, duration, release year about certain song.
+   Also you can discover which is my favorite.
+*/
+var songs = [
+  {
+    artist: "Maroon 5",
+    genre: "Pop",
+    title: "Girls like you",
+    durationInSeconds: 320,
+    releaseYear: 2017,
+    isFavorite: false
+  },
+  {
+    artist: "Slipknot",
+    genre: "Rock",
+    title: "Unsainted",
+    durationInSeconds: 305,
+    releaseYear: 2019,
+    isFavorite: true
+  },
+  {
+    artist: "The Weeknd",
+    genre: "Soul",
+    title: "Starboy",
+    durationInSeconds: 274,
+    releaseYear: 2016,
+    isFavorite: false
+  }
+]; // Songs collection.
